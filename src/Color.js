e06be7496c04584179435fdc9cbf08e7737d9320
Rayssa Costa <rayssascoosta@gmail.com>
2024-09-17 23:07:17 -0300

    Adiciona metodo que retorna a cor em ANSI escape codes

--- src/Color.js
+++ src/Color.js
@@ -3,6 +3,19 @@ class Color {
         this.index = index;
         this.colorName = colorName;
     }
+
+    getColorCode(colorName) {
+        return this.colorsCode[colorName];
+    }
+
+    colorsCode =  Object.freeze({
+        "red": "\x1b[31m",
+        "green": "\x1b[32m",
+        "yellow": "\x1b[33m",
+        "blue": "\x1b[34m",
+        "white": "\x1b[37m",
+        "orange": "\x1b[38;5;208m"
+    });   
 }
 
 module.exports = Color;
